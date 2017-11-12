d60f58cb55525b899eeb0cf7fe18bc85b640f94a
Alaa Moussawi <AlaaMoussawi@kamal-75.dynamic.rpi.edu>
2017-11-12 07:32:53 -0500

    Updated the code that converts airline code to name

--- get_airline_name.py
+++ get_airline_name.py
@@ -1,30 +1,30 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[11]:
+# In[4]:
 
 
 data_airlines = pd.read_csv("airline_codes.txt", sep = ',')
 
 
-# In[16]:
+# In[15]:
 
 
 def get_airline_name(two_letter_code):
-    return data_airlines[data_airlines['Code2'] == two_letter_code]['Name']
+    return data_airlines[data_airlines['Code2'] == two_letter_code]['Name'].values[0]
 
 
-# In[18]:
+# In[14]:
 
 
-get_airline_name('AA')
+get_airline_name('AA').values[0]
 
 
 # In[ ]:

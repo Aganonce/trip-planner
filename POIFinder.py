1c2606d7b7d1eb54b874503fed8cff880203d67f
mikeaggro <mikeaggro@gmail.com>
2017-11-12 05:26:13 -0500

    Add files via upload

---
+++ POIFinder.py
@@ -0,0 +1,88 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from urllib2 import Request, urlopen, URLError
+import json
+from geopy.geocoders import Nominatim
+
+
+# In[2]:
+
+APIKey="61ac3343-782f-40e6-ad1e-d8d14b2f1fe3"
+APIRootLocation="http://api.tripadvisor.com/api/partner/2.0/location/"
+APIRootMap="http://api.tripadvisor.com/api/partner/2.0/map/"
+
+
+# In[3]:
+
+def location_idRequest(Key,Root,ID): ##Returns JSON of specific location id
+    request=Request(str(Root)+str(LocationID)+"?key="+str(Key))
+    try:
+        response=urlopen(request)
+        response=response.read()
+        response=json.loads(response)
+        return response
+    except URLError,e:
+        print "Got Error Code: ",e
+
+
+# In[4]:
+
+def getCityCoord(City):
+    geolocator=Nominatim()
+    location=geolocator.geocode(City)
+    return location.latitude,location.longitude
+
+
+# In[36]:
+
+def cityRequest(City,Key,Root): ##returns list of non-hotels in top attractions with Name,WebURL,
+    Coords=getCityCoord(City) ##Get lat,long pair
+    request=Request(str(Root)+str(Coords[0])+","+str(Coords[1])+"?key="+str(Key))
+    try:
+        response=urlopen(request)
+        response=response.read()
+        response=json.loads(response)
+        #return response
+    except URLError,e:
+        print "Got Error Code:",e
+    RestAttract=[]
+    for i in range(len(response['data'])):
+        if response['data'][i]['category']['localized_name']!="Hotel" and response['data'][i]['rating']!=None:
+            #print response['data'][i]['category']['localized_name']
+            Name=response['data'][i]['name']
+            WebURL=response['data'][i]['web_url']
+            Rating=response['data'][i]['rating']
+            Photos=response['data'][i]['see_all_photos']
+            RestAttract.append([Name,WebURL,Rating,Photos])
+    return RestAttract
+
+
+# In[37]:
+
+City="Albany"
+Coord=getCityCoord(City)
+tmp=cityRequest(City,APIKey,APIRootMap) ##Returns top 10 results f
+
+
+# In[ ]:
+
+
+
+
+# In[19]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
